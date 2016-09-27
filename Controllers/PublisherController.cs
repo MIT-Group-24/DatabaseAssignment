2a7f24e99c571cd773444928d888d9bbf897ec97
Bont <tolutheo@gmail.com>
2016-09-27 10:28:12 +0000

    commit

---
+++ Controllers/PublisherController.cs
@@ -0,0 +1,64 @@
+﻿using BookShopInventoryApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using BookInventoryApp.DAL;
+
+namespace BookInventoryApp.Controllers
+{
+    public class PublisherController : Controller
+    {
+        private OperationDataContext context;
+        public PublisherController()
+        {
+            context = new OperationDataContext();
+        }
+
+        public ActionResult Index()
+        {
+            IList<PublisherModel> publisherList = new List<PublisherModel>();
+            var query = from publisher in context.Publishers
+                        select publisher;
+
+            var publishers = query.ToList();
+            foreach (var publisherData in publishers)
+            {
+                publisherList.Add(new PublisherModel()
+                {
+                    Id = publisherData.Id,
+                    Name = publisherData.Name,
+                   Year = Convert.ToInt32(publisherData.Year)
+                });
+            }
+            return View(publisherList);
+        }
+
+        public ActionResult Create()
+        {
+            PublisherModel model = new PublisherModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(PublisherModel model)
+        {
+            try
+            {
+                Publisher publisher = new Publisher()
+                {
+                    Name = model.Name,
+                    Year = model.Year.ToString()
+                };
+                context.Publishers.InsertOnSubmit(publisher);
+                context.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+    }
+}
---
+++ Models/BookModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookShopInventoryApp.Models
+{
+    public class BookModel
+    {
+        public BookModel()
+         {
+             Publishers = new List<SelectListItem>();
+         }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Year { get; set; }
+        public decimal Price { get; set; }
+        [DisplayName("Publisher")]
+        public int PublisherId { get; set; }
+        public string PublisherName { get; set; }
+        public IEnumerable<SelectListItem> Publishers { get; set; }
+    }
+    }
