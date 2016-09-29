2d73ba3073ebb3acf060ff4ee6b06b71ba433a4a
Bont <tolutheo@gmail.com>
2016-09-29 15:10:07 +0000

    validation added

--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,17 +16,23 @@ namespace BookShopInventoryApp.Models
          }
 
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Author { get; set; }
+        [Required]
         public string Year { get; set; }
+        [Required]
         public decimal Price { get; set; }
-        [DisplayName("Publisher")]
+        [DisplayName("Publisher"),Required]
         public int PublisherId { get; set; }
-        [DisplayName("Publisher")]
+        [DisplayName("Publisher"),Required]
         public string PublisherName { get; set; }
+        [Required]
         public IEnumerable<SelectListItem> Publishers { get; set; }
+        [Required]
         public string ISBN { get; set; }
-        [DisplayName("Stock Level")]
+        [DisplayName("Stock Level"),Required]
         public int StockLevel { get; set; }
     }
     }
--- Models/PublisherModel.cs
+++ Models/PublisherModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,10 +10,12 @@ namespace BookShopInventoryApp.Models
     public class PublisherModel
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int Year { get; set; }
+        [Required]
         public string Address { get; set; }
-        [DisplayName("Phone Number")]
+        [DisplayName("Phone Number"),Required]
         public string PhoneNumber { get; set; }
     }
 }
